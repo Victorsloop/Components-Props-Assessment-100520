2085e1efb3d6964c7d3dac4594d225c89b3e00b4
Victorsloop <torres.victor136@gmail.com>
2020-12-08 18:16:00 -0500

    Done

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 
 function Note(props) {
-    return <li>{props.note.content}</li>
+    // console.log(props)
+    return <li>{ props.note.content}</li>
+    // console.log(props)
 }
 
 export default Note;
--- src/Containers/NotesContainer.js
+++ src/Containers/NotesContainer.js
@@ -5,11 +5,16 @@ class NotesContainer extends React.Component {
     apiResponse = () => {
         return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
     }
-
+    // Went with the function route, using the this syntax and calling the method within the ul to implement it
+    renderNotes = () => {
+        return this.apiResponse().map(renderedNote => {
+            return <Note note = {renderedNote}/>
+        })
+    }
     render() {
         return (
             <ul>
-                {}
+                {this.renderNotes()}
             </ul>
         )
     }
